970ff0a91cf1a284de7e970491b074494ee19abb
Abdulkbk <abdoollahikbk@gmail.com>
2022-11-03 23:50:25 +0100

    Fixed database con and setup user structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersController } from './user/user.controller';
+import { UsersModule } from './user/user.module';
+
+console.log(process.env.MONGO_URI);
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), AuthModule],
-  controllers: [AppController],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    AuthModule,
+    UsersModule,
+  ],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
